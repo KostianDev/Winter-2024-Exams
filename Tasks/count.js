8bd9b596de8dc9d3abef05b169027cf00cdb2c59
KostianDev <143603709+KostianDev@users.noreply.github.com>
2024-01-14 22:55:51 +0200

    Update count.js

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,16 +1,19 @@
 // Sum all number values in dict
 
+// Step 1
+//
+// Remove unneeded code
+// Add 'use strict'
+
+'use strict'
+
 count = (obj) => {
   sum = 0;
-  {
-  }
   keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
   return sum;
 };
 
