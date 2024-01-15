320f193f5611da8783b1ba40fce0455e5c50ceda
KostianDev <143603709+KostianDev@users.noreply.github.com>
2024-01-15 04:52:23 +0200

    Update between.js

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,19 +1,18 @@
 // Extract substring between prefix and suffix
 
-// Step 6
+// Step 7
 //
-// Added variable to stop changing incoming parameters
+// Moved 'extractedString' to another lexical scope
+// Rewrite if...else to make code more compact
 
 'use strict'
 
 const getSubstringBetween = (string, prefix, suffix) => {
   const startIndex = string.indexOf(prefix) + prefix.length;
   const endIndex = string.indexOf(suffix);
-  if (startIndex === -1 || endIndex === -1) return '';
-  else {
-    const extractedString = string.substring(startIndex, endIndex);
-  }
-  return extractedString;
+  const extractedString = string.substring(startIndex, endIndex);
+  return startIndex === -1 || endIndex === -1 
+    ? '' : extractedString;
 };
 
 module.exports = getSubstringBetween;
