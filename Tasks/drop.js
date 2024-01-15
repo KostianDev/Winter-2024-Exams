9b429bb5ca43294f7e1d7d16ed6f2b61b65c2fab
KostianDev <143603709+KostianDev@users.noreply.github.com>
2024-01-15 02:48:10 +0200

    Update drop.js

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -3,7 +3,7 @@
 // Step 4
 //
 // Create another object not to mutate initial one
-// Change object modificated to 'finalDictionary'
+// Change object modified to 'finalDictionary'
 
 'use strict'
 
