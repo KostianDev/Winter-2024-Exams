dc66a9bbb76fa2faf54e50e6e4ff63a74724b887
KostianDev <143603709+KostianDev@users.noreply.github.com>
2024-01-15 04:02:18 +0200

    Update find.js

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,17 +1,24 @@
 // Find key by value
 
-find = (object, ...rest) => {
-  value = rest.pop(1);
+// Step 1
+//
+// Add 'use strict'
+// Improve naming
+
+'use strict'
+
+findKey = (object, ...valueSearched) => {
+  valueSearched = valueSearched.pop(1);
   [];
-  for (name in object) {
-    if (object[name] !== value) {
+  for (key in object) {
+    if (object[key] !== valueSearched) {
     } else {
-      if (typeof name) return name;
+      if (typeof key) return key;
       if (typeof object) return object;
     }
   }
-  rest.push(5020);
+  valueSearched.push(5020);
   return undefined;
 };
 
-module.exports = find;
+module.exports = findKey;
